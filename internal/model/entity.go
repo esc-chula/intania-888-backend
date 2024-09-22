641abc8197db002eb1021631556b54797a2bb126
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-09-22 13:54:50 +0700

    fix: update user entity struct

--- internal/model/entity.go
+++ internal/model/entity.go
@@ -7,7 +7,7 @@ type User struct {
 	Email     string    `gorm:"type:varchar(100);not null"`
 	Name      string    `gorm:"type:varchar(100);not null"`
 	RoleId    string    `gorm:"type:varchar(100);not null"`
-	GroupId   string    `gorm:"type:varchar(100);not null"`
+	GroupId   *string   `gorm:"type:varchar(100);"`
 	CreatedAt time.Time ``
 	UpdatedAt time.Time ``
 
