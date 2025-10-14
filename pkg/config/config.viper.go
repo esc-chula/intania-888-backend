641b445cdd082e504bde779ba45d66a037cc9bf8
RawSalmon <phanthawasjira@Gmail.com>
2025-10-14 17:50:06 +0700

    feat: env binding

--- pkg/config/config.viper.go
+++ pkg/config/config.viper.go
@@ -36,6 +36,9 @@ func NewViperConfig() Config {
 		default:
 			panic("Error: invalid app env")
 		}
+
+		// Bind environment variables to config keys
+		bindEnvVars(v)
 		v.AutomaticEnv()
 
 		if err := v.ReadInConfig(); err != nil {
@@ -100,4 +103,36 @@ func (c *viperConfig) GetSwagger() Swagger {
 
 func (c *viperConfig) GetCors() Cors {
 	return c.Cors
+}
+
+func bindEnvVars(v *viper.Viper) {
+	v.BindEnv("server_name", "SERVER_NAME")
+	v.BindEnv("server_env", "SERVER_ENV")
+	v.BindEnv("server_host", "SERVER_HOST")
+	v.BindEnv("server_port", "SERVER_PORT")
+	v.BindEnv("server_origin", "SERVER_ORIGIN")
+
+	v.BindEnv("db_host", "DB_HOST")
+	v.BindEnv("db_port", "DB_PORT")
+	v.BindEnv("db_user", "DB_USER")
+	v.BindEnv("db_pass", "DB_PASS")
+	v.BindEnv("db_name", "DB_NAME")
+	v.BindEnv("db_ssl_mode", "DB_SSL_MODE")
+	v.BindEnv("db_timezone", "DB_TIMEZONE")
+
+	v.BindEnv("cache_host", "CACHE_HOST")
+	v.BindEnv("cache_port", "CACHE_PORT")
+	v.BindEnv("cache_pass", "CACHE_PASS")
+
+	v.BindEnv("jwt_access_token_secret", "JWT_ACCESS_TOKEN_SECRET")
+	v.BindEnv("jwt_refresh_token_expiration", "JWT_REFRESH_TOKEN_EXPIRATION")
+
+	v.BindEnv("oauth_client_id", "OAUTH_CLIENT_ID")
+	v.BindEnv("oauth_client_secret", "OAUTH_CLIENT_SECRET")
+	v.BindEnv("oauth_redirect_uri", "OAUTH_REDIRECT_URI")
+
+	v.BindEnv("swagger_username", "SWAGGER_USERNAME")
+	v.BindEnv("swagger_password", "SWAGGER_PASSWORD")
+
+	v.BindEnv("cors_allow_origins", "CORS_ALLOW_ORIGINS")
 }
